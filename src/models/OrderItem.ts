5749b8d127f3f6ae833eaa43ed0a0ca7e2346628
Mr-Code-start <vitalybaca92@gmail.com>
2026-03-19 21:20:49 -0500

    feat(models): add model OrderItem

---
+++ src/models/OrderItem.ts
@@ -0,0 +1,10 @@
+import {Product} from "./Product";
+
+export class OrderItem {
+    constructor(
+        public product: Product,
+        public quantity: number,
+        public level: "PRIMARIA" | "SECUNDARIA"
+    ) {
+    }
+}
