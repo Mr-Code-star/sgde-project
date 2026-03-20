e3c031ab10db88216a24f7d3022493bcbf957c6a
Mr-Code-start <vitalybaca92@gmail.com>
2026-03-19 22:50:59 -0500

    refactor(models): delete file orderItem, Product and school

--- src/models/Product.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-    constructor(
-        public id: string,
-        public name: string,
-        public type: string, // cuaderno, polo, buzo
-        public size: string | null // S, M, L, XL, etc. (solo para ropa)
-    )
-    {}
-}
