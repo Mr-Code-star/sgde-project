e9f8da6f1b67bc3aa57253e67583a49e17531f5c
Mr-Code-start <vitalybaca92@gmail.com>
2026-03-20 13:24:59 -0500

    feat(config): add config of box-rules of level secundary

---
+++ src/config/box-rules-secundary.ts
@@ -0,0 +1,43 @@
+import {BoxRule} from "../models/BoxRule";
+
+export const BOX_RULES_SECUNDARY_CAJAS_CHICAS: Record<string, BoxRule> = {
+    // BUZO DELGADO
+    BUZO_DELGADO_TS: new BoxRule("S02","buzo_delgado_ts", 12, "SECUNDARIA"),
+    BUZO_DELGADO_TM: new BoxRule("S05", "buzo_delgado_tm", 11, "SECUNDARIA"),
+    BUZO_DELGADO_TL: new BoxRule("SO8", "buzo_delgado_tl", 10, "SECUNDARIA"),
+    BUZO_DELGADO_TXL: new BoxRule("S11", "buzo_delgado_txl", 10, "SECUNDARIA"),
+    // BUZO GRUESO
+    BUZO_GRUESO_TS: new BoxRule("S03","buzo_grueso_ts", 6, "SECUNDARIA"),
+    BUZO_GRUESO_TM: new BoxRule("S06", "buzo_grueso_tm", 6, "SECUNDARIA"),
+    BUZO_GRUESO_TL: new BoxRule("SO9", "buzo_grueso_tl", 6, "SECUNDARIA"),
+    BUZO_GRUESO_TXL: new BoxRule("S12", "buzo_grueso_txl", 6, "SECUNDARIA"),
+    // POLO
+    POLO_TS: new BoxRule("S01","polo_ts", 50, "SECUNDARIA"),
+    POLO_TM: new BoxRule("S04", "polo_tm", 50, "SECUNDARIA"),
+    POLO_TL: new BoxRule("SO7", "polo_tl", 48, "SECUNDARIA"),
+    POLO_TXL: new BoxRule("S10", "polo_txl", 48, "SECUNDARIA"),
+    // CUADERNOS
+    CUADERNO_CUADRICULADO: new BoxRule("S13","cuaderno_cuadriculado", 50, "SECUNDARIA"),
+    CUADERNO_RAYADO: new BoxRule("S14","cuaderno_rayado", 50, "SECUNDARIA"),
+}
+
+export const BOX_RULES_SECUNDARY_CAJAS_GRANDES: Record<string, BoxRule> = {
+    // BUZO DELGADO
+    BUZO_DELGADO_TS: new BoxRule("S02","buzo_delgado_ts", 28, "SECUNDARIA"),
+    BUZO_DELGADO_TM: new BoxRule("S05", "buzo_delgado_tm", 26, "SECUNDARIA"),
+    BUZO_DELGADO_TL: new BoxRule("SO8", "buzo_delgado_tl", 25, "SECUNDARIA"),
+    BUZO_DELGADO_TXL: new BoxRule("S11", "buzo_delgado_txl", 22, "SECUNDARIA"),
+    // BUZO GRUESO
+    BUZO_GRUESO_TS: new BoxRule("S03","buzo_grueso_ts", 22, "SECUNDARIA"),
+    BUZO_GRUESO_TM: new BoxRule("S06", "buzo_grueso_tm", 22, "SECUNDARIA"),
+    BUZO_GRUESO_TL: new BoxRule("SO9", "buzo_grueso_tl", 20, "SECUNDARIA"),
+    BUZO_GRUESO_TXL: new BoxRule("S12", "buzo_grueso_txl", 20, "SECUNDARIA"),
+    // POLO
+    POLO_TS: new BoxRule("S01","polo_ts", 140, "SECUNDARIA"),
+    POLO_TM: new BoxRule("S04", "polo_tm", 130, "SECUNDARIA"),
+    POLO_TL: new BoxRule("SO7", "polo_tl", 125, "SECUNDARIA"),
+    POLO_TXL: new BoxRule("S10", "polo_txl", 120, "SECUNDARIA"),
+    // CUADERNOS
+    CUADERNO_CUADRICULADO: new BoxRule("S13","cuaderno_cuadriculado", 50, "SECUNDARIA"),
+    CUADERNO_RAYADO: new BoxRule("S14","cuaderno_rayado", 50, "SECUNDARIA"),
+}
